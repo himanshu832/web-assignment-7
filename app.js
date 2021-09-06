5b36379ed8c4f83bfd0a460b524c608ad08da9f6
himanshu832 <84440871+himanshu832@users.noreply.github.com>
2021-09-06 23:05:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ btn.addEventListener("click",(e)=>{
     text.value="";
 });
 function getweather(input){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=cc7b365cdf72c6b1736b020dc6c87432`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=cc7b365cdf72c6b1736b020dc6c87432`)
     .then((result)=>{
         return result.json()
     })
